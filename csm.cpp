0b5eff2b42cfc1fb94f0735b0932bbb7d848559e
Erfan Mirshams <erfanmirshams@protonmail.com>
2023-04-11 13:04:49 +0330

    add the basics of needed classes

--- csm.cpp
+++ csm.cpp
@@ -5,11 +5,51 @@
 #include <sstream>
 
 #define FILE_SIZE 4
+#define DAYS_IN_MONTH 30
+#define HOURS_IN_DAY 24
 
 enum FILENAME {EMPLOYEE_FILE, SALARY_CONFIG_FILE, TEAMS_FILE, WORKING_HOURS_FILE};
+enum LEVEL {JUNIOR, EXPERT, SENIOR, TEAMLEAD};
 using namespace std;
 const string FILENAMES[] = {"employees.csv", "salary_configs.csv", "teams.csv", "working_hours.csv"};
 
+class Expertise{
+    private:
+        LEVEL level;
+        int baseSalary;
+        int salaryPerHour;
+        int salaryPerExtraHour;
+        int officialWorkingHours;
+        int taxPercentage;
+};
+
+class Employee{
+    public:
+        Employee();
+    private:
+        int id;
+        string name;
+        int age;
+        Expertise expertise;
+        int isWorking[DAYS_IN_MONTH][HOURS_IN_DAY];
+};
+
+class Team{
+    private:
+        int id;
+        int headId;
+        vector<Employee*> members;
+        int bonusMinWorkingHours;
+        double bonusWorkingHoursVariance;
+};
+
+class PedarSahab{
+    public:
+        PedarSahab();
+    private:
+        vector<Employee*> employees;
+        vector<Team*> teams;
+};
 vector<vector<string>> readCSV(string fileName){
    ifstream inputFile(fileName);
    vector<vector<string>> readedFile;
