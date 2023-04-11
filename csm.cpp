c9bdb1ed17da09afe9c29e82dac1c1eaf3f169e7
Erfan Mirshams <erfanmirshams@protonmail.com>
2023-04-11 14:19:16 +0330

    read employees

--- csm.cpp
+++ csm.cpp
@@ -7,13 +7,20 @@
 #define FILE_SIZE 4
 #define DAYS_IN_MONTH 30
 #define HOURS_IN_DAY 24
+#define ASSETS_FOLDER "assets/"
+#define EXPERTISE_SIZE 4
+#define EMPLOYEE_COLUMN_SIZE 4
 
-enum FILENAME {EMPLOYEE_FILE, SALARY_CONFIG_FILE, TEAMS_FILE, WORKING_HOURS_FILE};
+enum FILE_NAME {EMPLOYEE_FILE, SALARY_CONFIG_FILE, TEAMS_FILE, WORKING_HOURS_FILE};
 enum LEVEL {JUNIOR, EXPERT, SENIOR, TEAMLEAD};
+enum EMPLOYEE_COLUMN {EMP_ID, EMP_NAME, EMP_AGE, EMP_LEVEL};
 using namespace std;
-const string FILENAMES[] = {"employees.csv", "salary_configs.csv", "teams.csv", "working_hours.csv"};
+const string FILE_NAMES[] = {"employees.csv", "salary_configs.csv", "teams.csv", "working_hours.csv"};
+const string LEVEL_NAMES[] = {"junior", "expert", "senior", "team_lead"};
 
 class Expertise{
+    public:
+        void updateLevel(int level);
     private:
         LEVEL level;
         int baseSalary;
@@ -23,16 +30,27 @@ class Expertise{
         int taxPercentage;
 };
 
+void Expertise::updateLevel(int level){
+    this -> level = (LEVEL)level;
+}
+
 class Employee{
     public:
-        Employee();
+        Employee(int id, string name, int age, Expertise* expertise);
     private:
         int id;
         string name;
         int age;
-        Expertise expertise;
+        Expertise *expertise;
         int isWorking[DAYS_IN_MONTH][HOURS_IN_DAY];
 };
+
+Employee::Employee(int id, string name, int age, Expertise* expertise){
+    this -> id = id;
+    this -> name = name;
+    this -> age = age;
+    this -> expertise = expertise;
+}
 
 class Team{
     private:
@@ -46,10 +64,28 @@ class Team{
 class PedarSahab{
     public:
         PedarSahab();
+        Expertise *getExpertiseByInd(int ind);
+        void addEmployee(int id, string name, int age, Expertise* expertise);
     private:
         vector<Employee*> employees;
         vector<Team*> teams;
+        Expertise expertise[EXPERTISE_SIZE];
 };
+
+PedarSahab::PedarSahab(){
+    for(int i = 0; i < EXPERTISE_SIZE; i++){
+        expertise[i].updateLevel(i);
+    }
+}
+
+Expertise *PedarSahab::getExpertiseByInd(int ind){
+    return &(expertise[ind]);
+}
+
+void PedarSahab::addEmployee(int id, string name, int age, Expertise *expertise){
+    employees.push_back(new Employee(id, name, age, expertise));
+}
+
 vector<vector<string>> readCSV(string fileName){
    ifstream inputFile(fileName);
    vector<vector<string>> readedFile;
@@ -69,26 +105,74 @@ vector<vector<string>> readCSV(string fileName){
    return readedFile;
 }
 
+int getExpertiseToken(string tokenStr){
+    int token;
+    if(tokenStr == LEVEL_NAMES[JUNIOR]){
+        token = JUNIOR;
+    }
+    if(tokenStr == LEVEL_NAMES[EXPERT]){
+        token = EXPERT;
+    }
+    if(tokenStr == LEVEL_NAMES[SENIOR]){
+        token = SENIOR;
+    }
+    if(tokenStr == LEVEL_NAMES[TEAMLEAD]){
+        token = TEAMLEAD;
+    }
+    return token;
+}
 
-void readInput(){
+void readEmployees(vector<vector<string>> employeeReadFile, PedarSahab &pedarSahab){
+    for(int i = 0; i < (int)employeeReadFile.size(); i++){
+        int id;
+        string name;
+        int age;
+        Expertise *expertise;
+        for (int j = 0; j < EMPLOYEE_COLUMN_SIZE; j++) {
+            if(j == EMP_ID){
+                id = stoi(employeeReadFile[i][j]);
+            }
+            if(j == EMP_NAME){
+                name = employeeReadFile[i][j];
+            }
+            if(j == EMP_AGE){
+                age = stoi(employeeReadFile[i][j]);
+            }
+            if(j == EMP_LEVEL){
+                expertise = pedarSahab.getExpertiseByInd(getExpertiseToken(employeeReadFile[i][j]));
+            }
+        }
+        pedarSahab.addEmployee(id, name, age, expertise);
+    }
+}
+
+void readExpertise(vector<vector<string>> salaryConfigsReadFile, PedarSahab &pedarSahab){
+    for(int i = 0; i < (int)salaryConfigsReadFile.size(); i++){
+
+    }
+}
+
+PedarSahab readInput(){
+    PedarSahab pedarSahab;
+
     for(int i = 0; i < FILE_SIZE; i++){
         if(i == EMPLOYEE_FILE){
-            vector<vector<string>> employeeReadFile = readCSV(FILENAMES[i]);
-            //handling
+            readEmployees(readCSV(ASSETS_FOLDER + FILE_NAMES[i]), pedarSahab);
         }
         if(i == SALARY_CONFIG_FILE){
-            vector<vector<string>> salaryConfigsFile;
+            vector<vector<string>> salaryConfigsFile = readCSV(ASSETS_FOLDER + FILE_NAMES[i]);
             //handling
         }
         if (i == TEAMS_FILE){
-            vector<vector<string>> teamsFile;
+            vector<vector<string>> teamsFile = readCSV(ASSETS_FOLDER + FILE_NAMES[i]);
             //handling
         }
         if (i == WORKING_HOURS_FILE){
-            vector<vector<string>> workingHoursFile;
+            vector<vector<string>> workingHoursFile = readCSV(ASSETS_FOLDER + FILE_NAMES[i]);
             //handling
         }
     }
+    return pedarSahab;
 }
 
 int main(int argc, char *argv[]) {
